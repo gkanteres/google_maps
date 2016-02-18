d2ff7c16287b7f2aeac6b5596a9090e7f8c2654e
Gregory K and Illia Kuchko <ikuchko@gmail.com>
2016-02-18 11:02:19 -0800

    Add geolocation.

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,86 +1,98 @@
-var GMaps = require("gmaps");
 var destinationKey = "AIzaSyDp0QftGXRtod_pyaNZeQnMbJScGQ87F40";
 // var destinationKey = "AIzaSyDEf3D-N_JjVjOPBLwpmHCna-gegi5GcDs";
 var KEY = "e0aa4703629c3d46bd310eee601b23ff";
+var map;
+var myLatLng = {lat: -25.363, lng: 131.044};
+var bounds = new google.maps.LatLngBounds();
+
+var getMarkers = function (htmlId) {
+  for (var index in htmlId) {
+    var geocode = new google.maps.Geocoder;
+    geocode.geocode(
+      {
+        address: $('#' + htmlId[index]).val()
+      }, callback);
+    function callback(response, status) {
+      for (var i in response) {
+        var myLatLng = {lat: response[i].geometry.location.lat(), lng: response[i].geometry.location.lng()};
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!'
+        });
+        bounds.extend(marker.position);
+      }
+      map.fitBounds(bounds);
+    }
+  }
+}
 
 // Gmaps code needs to be in $(document).ready format in order to load properly
 $(function () {
 
-  var map;
-  var myLatLng = {lat: -25.363, lng: 131.044};
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: myLatLng,
-    zoom: 8
+    zoom: 5
   });
 
-  // var marker = new google.maps.Marker({
-  //   position: myLatLng,
-  //   map: map,
-  //   title: 'Hello World!'
-  // });
+  $("#current-location-btn").click(function() {
+    debugger;
+    var infoWindow = new google.maps.InfoWindow({map: map});
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+      }
+  });
+
+//   var infoWindow = new google.maps.InfoWindow({map: map});
+//
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//       var pos = {
+//         lat: position.coords.latitude,
+//         lng: position.coords.longitude
+//       };
+//
+//       infoWindow.setPosition(pos);
+//       infoWindow.setContent('Location found.');
+//       map.setCenter(pos);
+//     }, function() {
+//       handleLocationError(true, infoWindow, map.getCenter());
+//     });
+//   } else {
+//     // Browser doesn't support Geolocation
+//     handleLocationError(false, infoWindow, map.getCenter());
+//   }
+//
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(browserHasGeolocation ?
+//                         'Error: The Geolocation service failed.' :
+//                         'Error: Your browser doesn\'t support geolocation.');
+// };
+
 
   $(".map-location").submit(function(event){
     event.preventDefault();
-    map.removeMarkers();
-
-    var geocode = new google.maps.Geocoder;
-    geocode.geocode(
-      {
-        address: $('#address').val()
-      }, callback);
-
-      function callback(response, status) {
-        // console.log(response[0].geometry.location.lat());
-        for (var i in response) {
-          var myLatLng = {lat: response[i].geometry.location.lat(), lng: response[i].geometry.location.lng()};
-          debugger;
-          var marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            title: 'Hello World!'
-          });
-        }
-      }
-
+    getMarkers(["address"]);
   })
-
-
-  // var map = new GMaps({
-  //   div: '#map',
-  //   lat: -12.043333,
-  //   lng: -77.028333
-  // });
-  // $(".map-location").submit(function(event){
-  //   event.preventDefault();
-  //   GMaps.geocode({
-  //     address: $('#address').val(),
-  //     callback: function(results, status) {
-  //       if (status == 'OK') {
-  //         var latlng = results[0].geometry.location;
-  //         map.setCenter(latlng.lat(), latlng.lng());
-  //         map.removeMarkers();
-  //         map.addMarker({
-  //           lat: latlng.lat(),
-  //           lng: latlng.lng()
-  //         });
-  //       }
-  //     }
-  //   });
-  // });
 
   $(".map-destination").submit(function(event){
     event.preventDefault();
-    // var origin = $("#origin").val();
-    // var destination = $("#destination").val();
-    // $.get('http://api.openweathermap.org/data/2.5/weather?q=' + "Portland" + '&appid=' + KEY, function(response) {
-    //     console.log(response);
-    //   });
-    //  $.get('http://maps.googleapis.com/maps/api/distancematrix/json?origins=' + "Portland" + '&destinations=' + "Chicago" +"&key=" + destinationKey, function(response) {
-    //  console.log(response);
-    // });
-
-
     var origin1 = $("#origin").val();
     var destinationA = $("#destination").val();
 
@@ -100,7 +112,9 @@ $(function () {
       }, callback);
 
     function callback(response, status) {
-      // console.log(JSON.stringify(response));
+      getMarkers(["origin", "destination"]);
+      $('#destination-result').append('<p><em>Destination: </em>' + response.destinationAddresses + '<br><em>Trip Length: </em>' + response.rows[0].elements[0].duration.text + '<br><em>Trip Distance: </em>' + response.rows[0].elements[0].distance.text + '</p>');
+      console.log(response.rows[0].elements[0].duration.text)
     }
 
   });
